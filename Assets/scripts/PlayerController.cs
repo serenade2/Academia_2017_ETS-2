b67e93f48767ff174154a680a813433e0619c923
serenade2 <samuel.abraham.1@ens.etsmtl.ca>
2017-01-28 23:06:44 -0500

    -Added a basic networking connection. see https://unity3d.com/learn/tutorials/topics/multiplayer-networking.

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float HorizontalSensitivity;
+    public float VerticalSensitivity;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * HorizontalSensitivity;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * VerticalSensitivity;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
