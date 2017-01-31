6ffcf4e76667d8e63797002c92d42f9b332c0e58
Samuel <serenade.124@gmail.com>
2017-01-30 19:19:16 -0500

    -Added network synchronization for player movements:  - Attached a NetworkIdentity component to the player prefab  - Attached a NetworkTransform component to the player prefab -Created a bullet prefab

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
     public float HorizontalSensitivity;
     public float VerticalSensitivity;
+    public Color LocalPlayerColor;
+    public GameObject BulletPrefab;
+    public Transform BulletSpawn;
+    public float BulletVelocity;
+    /// <summary>
+    /// called on clients for player objects for the local client (only)
+    /// </summary>
+    public override void OnStartLocalPlayer()
+    {
+        //Add all initalization that are exclusive to the client (local machine) here. (Cameras and Inputs)
+
+        GetComponent<MeshRenderer>().material.color = LocalPlayerColor;
+    }
     // Use this for initialization
     void Start()
     {
@@ -15,10 +30,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // check if the current player that is processed is the same player as the local machine.
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        if ( Input.GetKeyDown(KeyCode.Space) )
+        {
+            Fire();
+        }
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * HorizontalSensitivity;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * VerticalSensitivity;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+    }
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        GameObject bullet = (GameObject)Instantiate(BulletPrefab, BulletSpawn.position, BulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * BulletVelocity;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
     }
 }
