da626c6aad8c7a646468492379f94830d14db002
Julien thibault <julienthibault244@gmail.com>
2017-02-01 21:17:11 -0500

    script update

--- Assets/scripts/UITargeting.cs
+++ Assets/scripts/UITargeting.cs
@@ -16,6 +16,8 @@ public class UITargeting : MonoBehaviour {
 
     private void LateUpdate()
     {
-        transform.LookAt(Camera.main.transform.position, -Vector3.down);
+       
+        transform.LookAt(Camera.main.transform.position);
+        transform.Rotate(Vector3.up, 180);
     }
 }
