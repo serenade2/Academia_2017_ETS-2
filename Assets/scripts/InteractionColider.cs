e0ce8b37bf10feed3393710b476985a5490587d3
Julien thibault <julienthibault244@gmail.com>
2017-02-01 20:51:56 -0500

    Ui objectif traking
    mais le canvas est a l'envert

---
+++ Assets/scripts/InteractionColider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionColider : MonoBehaviour {
+
+    public Canvas anCanvas;
+    public float ratio;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player") {
+            Debug.Log("stay");
+            if (Input.GetKey(KeyCode.E))
+            {
+                Debug.Log("E");
+                anCanvas.GetComponent<InteractionManager>().inProgress(ratio);
+            }
+            else {
+                anCanvas.GetComponent<InteractionManager>().abandonedProgress();
+            }
+        }
+        
+    }
+
+}
