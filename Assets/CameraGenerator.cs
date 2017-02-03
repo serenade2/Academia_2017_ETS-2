df58416798d3b2ae68b8107d379a953d491a4bc6
Molinette <m.monettemolina@gmail.com>
2017-02-02 20:49:36 -0500

    Ajout mécanique de camera

---
+++ Assets/CameraGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGenerator : MonoBehaviour {
+    public RoomCameras[] roomCamerasArray;
+    public Screens screensScript;
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] screens = screensScript.GetScreens();
+        int i = 0;
+        foreach (RoomCameras roomCameras in roomCamerasArray)
+        {
+            foreach (Camera camera in roomCameras.GetCameras())
+            {
+                RenderTexture rt = new RenderTexture(1024, 1024, 16, RenderTextureFormat.ARGB32);
+                rt.Create();
+                camera.enabled = true;
+                camera.targetTexture = rt;
+                GameObject screen = screens[i];
+                screens[i].GetComponent<Renderer>().material.mainTexture = rt;
+                i++;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Screens.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screens : MonoBehaviour{
+    public GameObject[] screens;
+
+    public GameObject[] GetScreens()
+    {
+        return screens;
+    }
+}
