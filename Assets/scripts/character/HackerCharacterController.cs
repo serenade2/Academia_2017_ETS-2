1991e029d4da4f20d1d9d01b090b53f63b1df465
Jean-Philippe Parent <jeanp.parent@gmail.com>
2017-02-13 01:18:47 -0500

    Add basic third person controller and camera

---
+++ Assets/scripts/character/HackerCharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ThirdPerson controller for the Hacker
+/// </summary>
+[RequireComponent(typeof(CharacterController))]
+public class HackerCharacterController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private CharacterController ctrl;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    this.ctrl = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal1"), 0f, Input.GetAxis("Vertical1"));
+	    ctrl.SimpleMove(input*speed*Time.deltaTime);
+	}
+}
