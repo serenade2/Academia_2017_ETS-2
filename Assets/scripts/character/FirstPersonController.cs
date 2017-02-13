ac7981e6bfdb86d6e9e439074fbcc6482f756575
serenade2 <samuel.abraham.1@ens.etsmtl.ca>
2017-02-13 00:02:53 -0500

    -added inputs for RT and LT buttons -Added inputs for Right stick -Added vertical clamping for Right stick -The camera can now move on the X axis while looking up and down around the z axis

--- Assets/scripts/character/FirstPersonController.cs
+++ Assets/scripts/character/FirstPersonController.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Debug = UnityEngine.Debug;
 
 public class FirstPersonController : MonoBehaviour
 {
     [Tooltip("The gameObject at witch the camera performs it's rotation")]
     public GameObject Target;
     public float height = 1.80f;
-
+    public float HorizontalSpeed = 5.0f;
+    public float RotationAngle = 2.0f;
     [SerializeField] private MouseLook m_MouseLook;
 
     private Camera m_Camera;
@@ -18,6 +22,8 @@ public class FirstPersonController : MonoBehaviour
     [Tooltip("The maximal value at which the camera stop moving further")]
     public float VerticalClamp;
     public bool InvertVerticalAxys = false;
+    private float m_rotationToPerform = 0.0f;
+    private bool m_performRotationAroundZ = false;
     private void Start()
     {
         m_Camera = GetComponentInChildren<Camera>();
@@ -55,44 +61,68 @@ public class FirstPersonController : MonoBehaviour
         float vertical1Input = Input.GetAxis("Vertical1");
         float rightJoyX1Input = Input.GetAxis("Right Joystick X 1");
         float rightJoyY1Input = Input.GetAxis("Right Joystick Y 1");
+        float RT1 = Input.GetAxis("RT 1");
+        float LT1 = Input.GetAxis("LT 1");
+        Quaternion cameraRotation = m_Camera.transform.rotation;
         Vector3 positiveRotation = new Vector3(0,0,1);
 
-        //if (horizontal1Input > 0.0f)
-        //{
-        //    m_Camera.transform.Translate(Vector3.right * Time.deltaTime);
-        //}
-        //else if (horizontal1Input < 0.0f)
-        //{
-        //    m_Camera.transform.Translate(Vector3.left * Time.deltaTime);
-        //}
-        //else if (vertical1Input > 0.0f)
-        //{
-        //    m_Camera.transform.Translate(Vector3.forward * Time.deltaTime);
-        //}
-        //else if (vertical1Input < 0.0f)
-        //{
-        //    m_Camera.transform.Translate(Vector3.back * Time.deltaTime);
-        //}
-        //else
-        //{
-        //    // dead zone
-        //    transform.Translate(Vector3.zero);
-        //}
+        if (horizontal1Input == 1.0f)
+        {
+            m_Camera.transform.Translate(Vector3.right * HorizontalSpeed * Time.deltaTime);
+        }
+        else if (horizontal1Input == -1.0f)
+        {
+            m_Camera.transform.Translate(Vector3.left * HorizontalSpeed * Time.deltaTime);
+        }
 
-        if (rightJoyY1Input > 0.0f && m_Camera.transform.rotation.x < VerticalClamp)
+        if (rightJoyY1Input == 1.0f)
         {
-            Debug.Log("RS+:" + rightJoyY1Input);
-            m_Camera.transform.RotateAround(Target.transform.position, Vector3.right, 20 * Time.deltaTime);
+            if (m_performRotationAroundZ)
+            {
+                m_rotationToPerform += (cameraRotation.z + RotationAngle);
+            }
+            else
+            {
+                m_rotationToPerform += (cameraRotation.x + RotationAngle);
+            }
            
-            //m_Camera.transform.Rotate(Vector3.right);
+
+            if (m_rotationToPerform > VerticalClamp)
+            {
+                m_rotationToPerform = VerticalClamp;
+            }
+
+            if (m_performRotationAroundZ)
+            {
+                Debug.Log(m_performRotationAroundZ);
+                m_Camera.transform.rotation = Quaternion.AngleAxis(m_rotationToPerform, Vector3.right);
+            }
+            else
+            {
+                m_Camera.transform.rotation = Quaternion.AngleAxis(m_rotationToPerform, Vector3.right);
+            }
         }
-        else if (rightJoyY1Input < 0.0f && m_Camera.transform.rotation.x > -VerticalClamp)
+        else if (rightJoyY1Input == -1.0f)
         {
-            //m_Camera.transform.Rotate(Vector3.left);
-            m_Camera.transform.RotateAround(Target.transform.position, Vector3.left, 20 * Time.deltaTime);
+            m_rotationToPerform += (cameraRotation.x + RotationAngle) * -1;
+        
+            if (m_rotationToPerform < -VerticalClamp)
+            {
+                m_rotationToPerform = -VerticalClamp;
+            }
+
+            m_Camera.transform.rotation = Quaternion.AngleAxis(m_rotationToPerform, Vector3.right);
         }
-     
-       
+        //else if (RT1 == 1.0f)
+        //{
+        //    m_Camera.transform.rotation = Quaternion.AngleAxis(90, Vector3.up);
+        //    m_performRotationAroundZ = !m_performRotationAroundZ;
+        //}
+        //else if (LT1 == 1.0f)
+        //{
+        //    m_Camera.transform.rotation = Quaternion.AngleAxis(90, Vector3.down);
+        //    m_performRotationAroundZ = !m_performRotationAroundZ;
+        //}
     }
 }
 
