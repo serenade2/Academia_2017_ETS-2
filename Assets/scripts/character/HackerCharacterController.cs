4f4ea1414987e899a15eb226cca681434f9e47de
serenade2 <samuel.abraham.1@ens.etsmtl.ca>
2017-02-22 18:57:12 -0500

    -Updated Hacker character controller -Added a new scene that spawn the hacker -Updated the build settings -Updated the hacker and hackerCamera prefabs -Added a camera manager for future use

--- Assets/scripts/character/HackerCharacterController.cs
+++ Assets/scripts/character/HackerCharacterController.cs
@@ -1,27 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Networking;
 /// <summary>
 /// ThirdPerson controller for the Hacker
 /// </summary>
 [RequireComponent(typeof(CharacterController))]
-public class HackerCharacterController : MonoBehaviour
+public class HackerCharacterController : NetworkBehaviour
 {
+    private bool canMove = true;
     public float speed = 10f;
-
     private CharacterController ctrl;
-
+    private Rigidbody rb;
 	// Use this for initialization
 	void Start ()
 	{
 	    this.ctrl = GetComponent<CharacterController>();
+	    //rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal1"), 0f, Input.GetAxis("Vertical1"));
-	    ctrl.SimpleMove(input*speed*Time.deltaTime);
+	    if (!hasAuthority)
+	    {
+	        return;
+	    }
+	    if (canMove)
+	    {
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal1"), 0f, Input.GetAxis("Vertical1"));
+            ctrl.SimpleMove(input * speed * Time.deltaTime);
+        }
 	}
+
+    public void Rewind(bool isRewinding)
+    {
+        if (isRewinding)
+        {
+            canMove = false;
+            //rb.isKinematic = true;
+        }
+        else
+        {
+            canMove = true;
+            //rb.isKinematic = false;
+        }
+    }
+
+    public void Pause(bool isPaused)
+    {
+
+    }
+
+    public void FastForward(bool fastForward)
+    {
+
+    }
 }
