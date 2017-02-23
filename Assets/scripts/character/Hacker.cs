3a56592a4732f0100074890d8d88aed79d5285bd
unknown <Conjure2@CONJURE043616.ens.ad.etsmtl.ca>
2017-02-23 10:38:01 -0500

    -Added linked list in hacker class

--- Assets/scripts/character/Hacker.cs
+++ Assets/scripts/character/Hacker.cs
@@ -1,20 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class Hacker : NetworkBehaviour
 {
-    public 
+    [Tooltip("The list that contains the Hackable AI")]
+    public LinkedList<GameObject> AiLinkedList;
+
+    private int _currentIndex;
 	// Use this for initialization
-	void Start ()
+	public override void OnStartLocalPlayer()
     {
-		
-	}
+		AiLinkedList = new LinkedList<GameObject>();
+	    _currentIndex = 0;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+	    if (Input.GetKeyDown(KeyCode.Joystick1Button4))
+	    {
+	        Debug.Log("LB2 Pressed");
+	    }
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button5))
+	    {
+	        Debug.Log("RB2 pressed");    
+	    }
 	}
+
+    public void AddAi(GameObject ai)
+    {
+        // check if the gameObject we add is an AICharacter
+        if (ai.GetComponent<AICharacter>() != null)
+        {
+            AiLinkedList.AddLast(ai);
+        }
+    }
+    public void RemoveAi(GameObject ai)
+    {
+        AiLinkedList.Remove(ai);
+    }
+
+    public void NextTarget()
+    {
+        if (_currentIndex < AiLinkedList.Count)
+        {
+            _currentIndex++;
+        }
+    }
+
+    public void PreviousTarget()
+    {
+        if (_currentIndex > AiLinkedList.Count)
+        {
+            _currentIndex--;
+        } 
+    }
+
+    public GameObject GetCurrentAi()
+    {
+        return AiLinkedList.ElementAt(_currentIndex);
+    }
 }
