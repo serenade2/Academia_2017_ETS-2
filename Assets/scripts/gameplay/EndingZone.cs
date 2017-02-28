4a21e58eb9117837a67666adffec07f8c78886d7
Jean-Philippe Parent <jeanp.parent@gmail.com>
2017-02-27 20:09:40 -0500

    Add basic game state manager

---
+++ Assets/scripts/gameplay/EndingZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingZone : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hacker")
+        {
+            GameStateManager.Instance.SetGameState(GameState.End);
+        }
+    }
+}
---
+++ Assets/scripts/gameplay/GameStateManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Holds the different game states
+/// </summary>
+public enum GameState
+{
+    None,
+    Intro,
+    MainMenu,
+    Game,
+    End
+}
+
+/// <summary>
+/// delegate which will be given by the caller of the SetGameState method
+/// </summary>
+public delegate void OnStateChangeHandler();
+
+/// <summary>
+/// Class that manages the game states and fires an event when transitioning
+/// </summary>
+public class GameStateManager : MonoBehaviour
+{
+    protected GameStateManager() { }        // make it protected so no one can instantiate it themselves
+    private static GameStateManager instance = null;
+    public event OnStateChangeHandler OnStateChange;
+
+    public GameState gameState { get; private set; }
+    public static bool isActive
+    {
+        get { return instance != null; }
+    }
+
+
+    public static GameStateManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<GameStateManager>();
+
+                if (instance == null)
+                {
+                    GameObject gameObject  = new GameObject("GameStateManager");
+                    DontDestroyOnLoad(gameObject);
+                    instance = gameObject.AddComponent<GameStateManager>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    public void SetGameState(GameState state)
+    {
+        this.gameState = state;
+        if (OnStateChange != null)
+        {
+            OnStateChange();
+        }
+       
+    }
+}
