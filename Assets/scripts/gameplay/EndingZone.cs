044a43854d9771c41eaa870b15edf302e79b4213
Jean-Philippe Parent <jeanp.parent@gmail.com>
2017-02-28 11:09:31 -0500

    Add event callback when entering the end zone

--- Assets/scripts/gameplay/EndingZone.cs
+++ Assets/scripts/gameplay/EndingZone.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class EndingZone : MonoBehaviour {
+public class EndingZone : MonoBehaviour
+{
+    // delegate function for the  OnStateChange event
+    private void OnStateChange()
+    {
+        Debug.Log("player wins");
+        Debug.Log("current state: " + GameStateManager.Instance.gameState);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Hacker")
+        if (other.tag != "Hacker") return;
+
+        GameStateManager.Instance.OnStateChange += OnStateChange;
+        GameStateManager.Instance.SetGameState(GameState.End);
+    }
+
+    void OnDestroy()
+    {
+        // remove the delegate reference to avoid memory leaks
+        if (GameStateManager.isActive)
         {
-            GameStateManager.Instance.SetGameState(GameState.End);
+            GameStateManager.Instance.OnStateChange -= OnStateChange;
         }
     }
 }
