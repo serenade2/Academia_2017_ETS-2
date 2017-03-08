11924984c5fcf09e88fe3e19c73848980473d28f
serenade2 <samuel.abraham.1@ens.etsmtl.ca>
2017-03-07 23:56:52 -0500

    -Added a command and a function that spawn a decoy  and destroy it at a specified lifetime

--- Assets/scripts/character/TakeOver.cs
+++ Assets/scripts/character/TakeOver.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -10,7 +11,7 @@ public class TakeOver : NetworkBehaviour
     public List<GameObject> AiList;
     [Tooltip("The radius that allow the hacker to take control of AI")]
     public float HackingRadius;
-    public float DecoyLifeTime;
+    public float DecoyLifeTime = 5.0f;
     public int _currentIndex;
     public GameObject CursorPrefab;
     private HackerCursor hackerCursor;
@@ -108,14 +109,10 @@ public class TakeOver : NetworkBehaviour
         {
             _currentIndex ++;
 
-            if(_currentIndex == AiList.Count)
+            if(_currentIndex >= AiList.Count)
             {
                 _currentIndex = 0;
             }
-        }
-        else
-        {
-            _currentIndex = 0;
         }
     }
 
@@ -159,17 +156,84 @@ public class TakeOver : NetworkBehaviour
         Vector3 targetWalkingDirection = targetGameObject.transform.forward;
         Transform targetTransform = targetGameObject.transform;
 
+        //TODO add an accessor in the class that use the NavMeshAgent to retreive the original ai speed 
+        SpawnDecoy(this.gameObject, targetTransform.position, targetTransform.rotation, DecoyLifeTime, 50.0f);
+        CmdSpawnDecoy(this.gameObject, targetTransform.position, targetTransform.rotation, DecoyLifeTime, 50.0f);
+
         // Update on the client the mesh
         UpdateHackerMesh(targetGameObject);
         // update on the server the mesh
         CmdUpdateHackerMesh(targetGameObject);
 
-        // Destroy the hacked target on the server and sync it on all the clients
-        CmdDestroyAI(targetGameObject);
+        try
+        {
+            // Destroy the hacked target on the server and sync it on all the clients
+            CmdDestroyAI(targetGameObject);
+        }
+        catch (Exception ex)
+        {
+            print(ex.Message);
+        }
+
         // move the hacker at the same position of the ai
         this.transform.position = targetTransform.position;
         this.transform.rotation = targetTransform.rotation;
         this.transform.localScale = targetTransform.localScale;
+    }
+
+
+    /// <summary>
+    /// Spawn a fake Agent that walk in the direction of the previous agent
+    /// before he desintegrate.
+    /// </summary>
+    /// <param name="decoy">The decoy agent that is created in the scene</param>
+    /// <param name="target">The direction at which the decoy will walk towards</param>
+    /// <param name="lifeTime">The time elapsed before the decoy dies</param>
+    /// <param name="walkSpeed">The speed at which the decoy will move</param>
+    public void SpawnDecoy(GameObject decoy, Vector3 targetPosition, Quaternion targetQuaternion, float lifeTime, float walkSpeed)
+    {
+        GameObject spawnedDecoy = GameObject.Instantiate(decoy);
+        
+        spawnedDecoy.name = "DECOY_" + decoy.name;
+        spawnedDecoy.transform.parent = null;
+        DetachDecoyComponents(spawnedDecoy);    
+
+        Vector3 direction = targetPosition - decoy.transform.position;
+        spawnedDecoy.transform.rotation = targetQuaternion;
+        //TODO make the decoy walk in the same direction the original agent was facing.
+        spawnedDecoy.transform.Translate(direction * walkSpeed * Time.deltaTime);
+        //TODO play the desintegrate animation
+
+        // then eliminate the decoy!
+        try
+        {
+            Destroy(spawnedDecoy, lifeTime);
+        }
+        catch (System.Exception ex)
+        {
+            print(ex.Message);
+        }
+    }
+
+    [Command]
+    public void CmdSpawnDecoy(GameObject decoy, Vector3 targetPosition, Quaternion targetQuaternion, float lifeTime, float walkSpeed)
+    {
+        SpawnDecoy(decoy, targetPosition, targetQuaternion, lifeTime, walkSpeed);
+    }
+
+    private void DetachDecoyComponents(GameObject spawnedDecoy)
+    {
+        try
+        {
+            Destroy(spawnedDecoy.GetComponentInChildren<SphereCollider>());
+            Destroy(spawnedDecoy.GetComponent<TakeOver>());
+            Destroy(spawnedDecoy.GetComponent<HackerCharacterController>());
+            spawnedDecoy.GetComponent<CharacterController>().enabled = false;
+        }
+        catch(System.Exception ex)
+        {
+            print(ex.Message);
+        }
     }
     
     [Command]
@@ -190,15 +254,7 @@ public class TakeOver : NetworkBehaviour
     [Command]
     public void CmdUpdateHackerMesh(GameObject targetGameObject)
     {
-        MeshRenderer targetMeshRenderer = targetGameObject.GetComponent<MeshRenderer>();
-        MeshFilter targetMeshFilter = targetGameObject.GetComponent<MeshFilter>();
-
-        MeshRenderer currentMeshRenderer = GetComponent<MeshRenderer>();
-        MeshFilter currentMeshFilter = GetComponent<MeshFilter>();
-
-        // take the apparency of the AI
-        currentMeshRenderer.materials = targetMeshRenderer.materials;
-        currentMeshFilter.mesh = targetMeshFilter.mesh;
+        UpdateHackerMesh(targetGameObject);
     }
 
     public void UpdateHackerMesh(GameObject targetGameObject)
@@ -211,7 +267,7 @@ public class TakeOver : NetworkBehaviour
 
         // take the apparency of the AI 
         currentMeshRenderer.materials = targetMeshRenderer.materials;
-       currentMeshFilter.mesh = targetMeshFilter.mesh;
+        currentMeshFilter.mesh = targetMeshFilter.mesh;
     }
 
     public void UpdateHackerCursor(bool visibleCondition)
