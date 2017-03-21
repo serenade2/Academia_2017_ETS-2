115926789b089947bd996f6496106b3fb74447c7
MSI\Conjure <m.monettemolina@gmail.com>
2017-03-21 19:28:41 -0400

    Mode drone ajouté

--- Assets/scripts/character/WatcherCameraController.cs
+++ Assets/scripts/character/WatcherCameraController.cs
@@ -11,7 +11,7 @@ public class WatcherCameraController : MonoBehaviour
     public float radius = 1f;
     public float moveSpeed = 1;
     public float lookAngleIncrement = 90;
-    public enum CameraMode {Rotation, Fps};
+    public enum CameraMode {Rotation, Fps, Drone};
     public CameraMode cameraMode = CameraMode.Fps; 
 
     // Use this for initialization
@@ -38,8 +38,16 @@ public class WatcherCameraController : MonoBehaviour
         {
             transform.RotateAround(level.transform.position, Vector3.up, -Input.GetAxis("Horizontal1") * 90 * Time.deltaTime);
             transform.RotateAround(level.transform.position, transform.right, Input.GetAxis("Vertical1") * 90 * Time.deltaTime);
-            transform.Translate((transform.position - level.transform.position) * Input.GetAxis("Zoom") * moveSpeed * Time.deltaTime, Space.World);
+            transform.Translate(transform.forward * Input.GetAxis("Zoom") * moveSpeed * Time.deltaTime, Space.World);
             transform.LookAt(level.transform.position);
+        }
+        else if(cameraMode == CameraMode.Drone)
+        {
+            transform.Rotate(Vector3.up, Input.GetAxis("RHorizontal1") * lookAngleIncrement * Time.deltaTime, Space.World);
+            transform.Rotate(transform.right, -Input.GetAxis("RVertical1") * lookAngleIncrement * Time.deltaTime, Space.World);
+            transform.Translate(new Vector3(transform.forward.x, 0, transform.forward.z).normalized * Input.GetAxis("Vertical1") * moveSpeed * Time.deltaTime, Space.World);
+            transform.Translate(transform.right * Input.GetAxis("Horizontal1") * moveSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.up * Input.GetAxis("Zoom") * moveSpeed * Time.deltaTime, Space.World);
         }
         else
         {
