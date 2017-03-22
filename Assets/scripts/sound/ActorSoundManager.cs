5b90a17cfb3d7d84ae67e3707dc96d7f0eb0eb0b
serenade2 <samuel.abraham.1@ens.etsmtl.ca>
2017-03-22 00:43:19 -0400

    -Created a ActorSoundManager script -Attached the ActorSoundManager on all the AI prefabs and TakeOver models

---
+++ Assets/scripts/sound/ActorSoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ActorSoundManager : MonoBehaviour
+{
+    public List<AudioClip> StepAudioClips;
+    public AudioClip InteractionAudioClip;
+    public AudioClip DesintegrateAudioClip;
+
+    private NetworkAnimator networkAnimator;
+	// Use this for initialization
+	void Start ()
+    {
+
+         networkAnimator = GetComponent<NetworkAnimator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
