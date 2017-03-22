99c59ed90f4c592c1d9480b0ba2e6d11353a48fe
Julien thibault <julienthibault244@gmail.com>
2017-03-21 21:57:39 -0400

    observer

---
+++ Assets/scripts/util/Observer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomUtile
+{
+    public interface Observer
+    {
+        void update();
+    }
+}
